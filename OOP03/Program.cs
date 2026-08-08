6fdd5c3a440a26e545e562e411eb7c7abad7457d
nourmuhammad632-maker <nourmuhammad632@gmail.com>
2026-08-09 02:34:53 +0300

    Question1

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -4,12 +4,41 @@
     {
         static void Main(string[] args)
         {
+            #region Question1
+            ////Part 01 — Theoretical Questions
+            //// a) What is the difference between Method Overloading and Method Overriding?
+            ////overloading :is the same method with the same logic diferrence is type and order and count of parameter 
+            ////overriding :redefining an inherited method in the child class with a new implementation
+
+            ////==============================================
+            ////b) What is the difference between Static Binding and Dynamic Binding?
+            ////static binding:"compile time polymorhism "is in method overloading
+            ////dynamic binding :"run time polymorphism "method overriding
+
+            ////====================================================
+            ////Q2 Sealed Classes and Methods
+            ////a) What is the purpose of the sealed keyword when applied to a class?
+            ////prevent ihertance
+
+            ////=================================================
+
+            ////b) What is the difference between a sealed class and a sealed method?
+            ////sealed class prevent ihertance from it
+            ////selaed method prevent override about it
+
+            ////========================================
+
+            ////c) Can a sealed method be overridden? Why?
+            ////no,sealed class prevented the overriden
+
+            #endregion
 
 
 
 
 
-           
+
+
         }
     }
 }
